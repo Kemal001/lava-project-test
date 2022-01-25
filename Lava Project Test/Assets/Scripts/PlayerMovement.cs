fdcd29a19e96c4a70c834b74a6197c6319536046
Kemal Segizbayev <kemal.segizbayev@gmail.com>
2022-01-26 01:37:27 +0600

    Player movement

---
+++ Lava Project Test/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Camera cam;
+
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        ClickToMove();
+    }
+
+    private void ClickToMove()
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            RaycastHit hit;
+
+            if(Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit, 500))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
