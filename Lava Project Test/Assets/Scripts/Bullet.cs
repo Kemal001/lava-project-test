eabba3366c8db5ebe14e3cd9bfff07702d7b6b45
Kemal Segizbayev <kemal.segizbayev@gmail.com>
2022-01-28 03:09:32 +0600

    Player Shooting

---
+++ Lava Project Test/Assets/Scripts/Bullet.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float explosionForce;
+    public float explosionRadius;
+
+    public float collisionRadius;
+
+    public LayerMask whatIsDamagable;
+
+    private Collider[] colliders;
+
+    private void Awake()
+    {
+        Destroy(gameObject, 3f);
+    }
+
+    private void Update()
+    {
+        colliders = Physics.OverlapSphere(transform.position, collisionRadius, whatIsDamagable);
+
+        foreach (Collider col in colliders)
+        {
+            Rigidbody[] rbs = col.GetComponentsInChildren<Rigidbody>();
+
+            foreach (Rigidbody rb in rbs)
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, collisionRadius);
+    }
+}
--- Lava Project Test/Assets/Scripts/PlayerShooting.cs
+++ Lava Project Test/Assets/Scripts/PlayerShooting.cs
@@ -3,20 +3,61 @@ using UnityEngine.AI;
 
 public class PlayerShooting : MonoBehaviour
 {
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    public Transform groundCheck;
+    public float radius;
+    public LayerMask whatIsShootingArea;
+
     private NavMeshAgent agent;
+
+    public float bulletSpeed;
+    public float fireRate;
+    private float lastShot = 0.0f;
+
+    public bool onShootingArea;
 
     private void Awake() => agent = GetComponent<NavMeshAgent>();
 
     private void Update()
     {
+        onShootingArea = Physics.CheckSphere(groundCheck.position, radius, whatIsShootingArea);
+
         RaycastHit hit;
 
         if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 500))
+        {
+            RotateTo(hit);
+            Shoot(hit);
+        }
+    }
+
+    private void RotateTo(RaycastHit hit)
+    {
+        if (onShootingArea)
         {
             Vector3 lookPos = hit.point - transform.position;
             lookPos.y = 0;
             Quaternion rotation = Quaternion.LookRotation(lookPos);
             transform.rotation = Quaternion.Lerp(transform.rotation, rotation, 100 * Time.deltaTime);
         }
+    }
+
+    private void Shoot(RaycastHit hit)
+    {
+        if (onShootingArea && Input.GetKey(KeyCode.Mouse0) && Time.time > fireRate + lastShot)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody>().AddForce((hit.point - firePoint.position) * bulletSpeed, ForceMode.Impulse);
+
+            lastShot = Time.time;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundCheck.position, radius);
     }
 }
